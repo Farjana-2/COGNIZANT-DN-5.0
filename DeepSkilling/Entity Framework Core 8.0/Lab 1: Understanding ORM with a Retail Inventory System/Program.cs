2e0bf469c4a647a325ef0105f1268b58a0099e78
Shaik Farjana <shaikfarjana0212@gmail.com>
2026-07-17 21:50:34 +0530

    Add files via upload

---
+++ DeepSkilling/Entity Framework Core 8.0/Lab 1: Understanding ORM with a Retail Inventory System/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using (var context = new RetailContext())
+{
+    context.Database.EnsureCreated();
+
+    var category = new Category { Name = "Electronics" };
+    var product = new Product { Name = "Wireless Mouse", Price = 19.99m, QuantityInStock = 50, Category = category };
+
+    context.Categories.Add(category);
+    context.Products.Add(product);
+    context.SaveChanges();
+
+    var products = context.Products.Include(p => p.Category).ToList();
+    foreach (var p in products)
+    {
+        Console.WriteLine($"{p.Name} - {p.Price:C} - Category: {p.Category.Name}");
+    }
+}
---
+++ DeepSkilling/Entity Framework Core 8.0/Lab 1: Understanding ORM with a Retail Inventory System/RetailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class RetailContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RetailInventoryDb;Trusted_Connection=True;");
+    }
+}
