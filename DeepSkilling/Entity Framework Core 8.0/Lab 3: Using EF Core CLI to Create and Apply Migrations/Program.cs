f670e5be1493a66e1c7662f87f7a3b7af554e159
Shaik Farjana <shaikfarjana0212@gmail.com>
2026-07-21 20:57:24 +0530

    Add files via upload

---
+++ DeepSkilling/Entity Framework Core 8.0/Lab 3: Using EF Core CLI to Create and Apply Migrations/Program.cs
@@ -0,0 +1,9 @@
+﻿using Lab3_Migrations.Data;
+
+Console.WriteLine("Retail Inventory System");
+Console.WriteLine();
+
+using InventoryContext context = new InventoryContext();
+
+Console.WriteLine("Inventory Context Created Successfully!");
+Console.WriteLine("Ready to create migrations.");
