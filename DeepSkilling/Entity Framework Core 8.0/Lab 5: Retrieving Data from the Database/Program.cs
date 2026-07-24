ae00aab530ca8c06fa95df97b506a8a27e64b14d
Shaik Farjana <shaikfarjana0212@gmail.com>
2026-07-24 14:35:23 +0530

    Add files via upload

---
+++ DeepSkilling/Entity Framework Core 8.0/Lab 5: Retrieving Data from the Database/Program.cs
@@ -0,0 +1,18 @@
+﻿using Lab3_Migrations.Data;
+using Lab3_Migrations.Models;
+
+using InventoryContext context = new InventoryContext();
+
+List<Product> products = context.Products.ToList();
+
+Console.WriteLine("Retail Inventory System");
+Console.WriteLine();
+
+foreach (Product product in products)
+{
+    Console.WriteLine("Product ID   : " + product.ProductId);
+    Console.WriteLine("Product Name : " + product.ProductName);
+    Console.WriteLine("Price        : " + product.Price);
+    Console.WriteLine("Quantity     : " + product.Quantity);
+    Console.WriteLine();
+}
