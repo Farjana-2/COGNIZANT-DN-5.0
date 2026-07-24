c51ad60a4c17b9aa4bedbcb0bff7affaee10065b
Shaik Farjana <shaikfarjana0212@gmail.com>
2026-07-24 13:38:30 +0530

    Add files via upload

---
+++ DeepSkilling/Entity Framework Core 8.0/Lab 4: Inserting Initial Data into the Database/InventoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lab3_Migrations.Models;
+
+namespace Lab3_Migrations.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+               @"Server=localhost;Database=RetailInventoryDB_Lab3;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ DeepSkilling/Entity Framework Core 8.0/Lab 4: Inserting Initial Data into the Database/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lab3_Migrations.Data;
+using Lab3_Migrations.Models;
+
+using InventoryContext context = new InventoryContext();
+
+Product product1 = new Product();
+product1.ProductName = "Laptop";
+product1.Price = 50000;
+product1.Quantity = 5;
+
+Product product2 = new Product();
+product2.ProductName = "Mouse";
+product2.Price = 800;
+product2.Quantity = 20;
+
+Product product3 = new Product();
+product3.ProductName = "Keyboard";
+product3.Price = 1500;
+product3.Quantity = 10;
+
+context.Products.Add(product1);
+context.Products.Add(product2);
+context.Products.Add(product3);
+
+
+context.SaveChanges();
+
+Console.WriteLine("Products inserted successfully!");
