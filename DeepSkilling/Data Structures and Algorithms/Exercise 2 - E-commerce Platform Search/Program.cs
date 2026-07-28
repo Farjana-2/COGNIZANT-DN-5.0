7f393f20fed57b73730104908d582c1459ea2190
Shaik Farjana <shaikfarjana0212@gmail.com>
2026-07-28 22:10:48 +0530

    Add files via upload

---
+++ DeepSkilling/Data Structures and Algorithms/Exercise 2 - E-commerce Platform Search/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace EcommerceSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Sorted array for Binary Search
+            Product[] products =
+            {
+                new Product(101,"Laptop","Electronics"),
+                new Product(102,"Mobile","Electronics"),
+                new Product(103,"Shoes","Fashion"),
+                new Product(104,"Watch","Accessories"),
+                new Product(105,"Camera","Electronics")
+            };
+
+
+            Console.WriteLine("Linear Search Result:");
+
+            Product result1 = SearchOperations.LinearSearch(products, 103);
+
+            if (result1 != null)
+            {
+                result1.Display();
+            }
+            else
+            {
+                Console.WriteLine("Product not found");
+            }
+
+
+
+            Console.WriteLine("\nBinary Search Result:");
+
+            Product result2 = SearchOperations.BinarySearch(products, 104);
+
+            if (result2 != null)
+            {
+                result2.Display();
+            }
+            else
+            {
+                Console.WriteLine("Product not found");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DeepSkilling/Data Structures and Algorithms/Exercise 2 - E-commerce Platform Search/SearchOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EcommerceSearch
+{
+    public class SearchOperations
+    {
+
+        // Linear Search
+        public static Product LinearSearch(Product[] products, int productId)
+        {
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].ProductId == productId)
+                {
+                    return products[i];
+                }
+            }
+
+            return null;
+        }
+
+
+        // Binary Search
+        public static Product BinarySearch(Product[] products, int productId)
+        {
+            int left = 0;
+            int right = products.Length - 1;
+
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+
+                if (products[middle].ProductId == productId)
+                {
+                    return products[middle];
+                }
+
+
+                if (products[middle].ProductId < productId)
+                {
+                    left = middle + 1;
+                }
+
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+
+            return null;
+        }
+    }
+}
